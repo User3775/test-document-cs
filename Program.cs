08aadb152e206b1ed3ba50738811f10c1bf82f90
Sarthak Sarkar <22SSarkar@heckgrammar.co.uk>
2024-12-10 09:51:58 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace test_document_cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+ 
